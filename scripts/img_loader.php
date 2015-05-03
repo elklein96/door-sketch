d9aca06b6b4572dcace41254ab186478e8e0b4ee
elklein96 <elklein96@gmail.com>
2015-05-02 22:22:44 -0400

    dumbdumbdumbdumbdumb

--- scripts/img_loader.php
+++ scripts/img_loader.php
@@ -4,9 +4,10 @@ try {
 	$m = new Mongo();
 	$db = $m->doors;
 	$collection = $db->submissions;
+	$id = uniqid();
 
 	if(isset($_POST['pic']))
-		addToMongo($_POST['pic'], uniqid());
+		addToMongo(array('id'=>$id, 'pic'=>$_POST['pic']), $id);
 	else
 		error_log("No data received");
 
@@ -22,7 +23,7 @@ function addToMongo($document, $id){
 	$idQuery = array('id' => $id);
 	if($collection->find($idQuery)->count() == 0){
 		error_log("Added new picture");
-		$collection->insert(json_decode($document));
+		$collection->insert($document);
 	}
 	else
 		error_log("Picture already exists");
