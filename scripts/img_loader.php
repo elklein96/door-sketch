c3bfeb5ea188506db29a33f6c8f8b800dac43abf
elklein96 <elklein96@gmail.com>
2015-05-04 11:16:42 -0400

    image merge

--- scripts/img_loader.php
+++ scripts/img_loader.php
@@ -26,7 +26,12 @@ function generatePic($pic){
 	imagealphablending($top_image, true); 
 	imagecopy($bottom_image, $top_image, 0, 0, 0, 0, imagesx($top_image), imagesy($top_image)); 
 
-	$output = "data:image/jpg;base64,".base64_encode($bottom_image);
+	ob_start($bottom_image); 
+	imagejpeg($img);
+	$image_data = ob_get_contents(); 
+	ob_end_clean(); 
+
+	$output = "data:image/jpg;base64,".base64_encode($image_data);
 
 	/*$overlay->setImageColorspace(Imagick::COLORSPACE_SRGB);
 	$image->setImageColorspace(Imagick::COLORSPACE_SRGB); 
